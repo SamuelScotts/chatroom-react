593ce487d527273b31836dbcc32567b7486381f4
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2021-02-17 18:39:02 +0000

    Imported Firebase / Setup .Env Files / Protected Routing

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,12 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Chatroom from './components/Chatroom';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+function PrivateRoute({ component: Component, authenticated, ...rest}){
+  return(
+    <Route {...rest} render={props => authenticated === true ? ( <Component {...props} /> ) : ( <Redirect to={{ pathname: '/', state: { from: props.location }}} />)} />
+  )
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest}){
+  return(
+    <Route {...rest} render={props => authenticated === false ? ( <Component {...props} /> ) : ( <Redirect to={{ pathname: '/chatroom', state: { from: props.location }}} />)} />
+  )
+}
 
 function App() {
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
-      <Login />
-      <Chatroom />
+      <Router>
+        <Switch>
+          <PublicRoute exact path="/" authenticated={authenticated} component={Login} />
+          <PrivateRoute path="/chatroom" authenticated={authenticated} component={Chatroom} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- chatroom/src/components/Login.js
+++ chatroom/src/components/Login.js
@@ -1,9 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import React, { useState } from 'react';
+
 
 function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function loginButton (){
+    
+        firebase.auth().signInWithEmailAndPassword(email, password)
+
+    }
+
     return(
-        <div>
-            <h1>Login</h1>
+        <div style={{ display: 'flex', flex: 1, height: '100vh' }}>
+            <div style={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', minWidth: 300 }}>
+                <input onChange={event => setEmail(event.target.value)} type="text" placeholder="Email" style={{marginRight: 20, paddingTop: 2, paddingBottom: 2, paddingRight: 24 }}></input>
+                <input onChange={event => setPassword(event.target.value)} type="password" placeholder="Password" style={{marginRight: 20, paddingTop: 2, paddingBottom: 2, paddingRight: 24 }}></input>
+                <button style={{ backgroundColor: 'black', borderRadius: 5, borderWidth: 0, paddingTop: 2, paddingBottom: 2, paddingLeft: 24, paddingRight: 24, 
+                 color: 'white', fontSize: 18, fontWeight: 'bold',}}
+                 onClick={loginButton}>Login</button>
+            </div>
         </div>
+        
     )
 }
 
